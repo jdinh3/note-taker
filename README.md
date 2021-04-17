845a06c6911431e96f96d20a85f7e3a5ec197588
Jenny Dinh <jenny.p.dinh@gmail.com>
2021-04-16 19:31:07 -0500

    updated routes

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 ## Description
   This application allows a user to take notes and save them within the application. The home page includes a button that directs the user to the notes page where the user will be able to type in notes and a title. When the save button is pressed, the notes will be saved to the side of the page and will persist when the page is refreshed.
 
-Here is a video running through the tests. [Test Tutorial](https://drive.google.com/file/d/1ywgA0ll7AMzMlmRqsZTGzxeervxEobOH/view)
+  Here are screen shots of the application. [Homepage](assets/localhost_8080_(webpage).png)
+  [Notes Page](assets/localhost_8080_notes(webpage)%20(1).png)
+
+Here is the deployed website. [Site]()
+
--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -1,32 +1,26 @@
 const router = require("express").Router();
-
 const fs = require("fs");
+
 router.get("/api/notes", (req, res) => {
   //read db.json and return it through response from server res.json()
   //data is retrieved as a string
-  let data = fs.readFileSync("./db/db.json", "utf8");
-  data = JSON.parse(data);
-  
+  let data = fs.readFileSync("db/db.json");
+
+  let dataParse = JSON.parse(data);
+
   //we need to send json back though
   //res.json() only sends back a json but the JSON.parse is actually parsing the data string into a json object
-  return res.json(data);
+  res.json(dataParse);
 });
 
 router.post("/api/notes", (req, res) => {
   let newNote = req.body;
-  let data = fs.readFileSync("./db/db.json", "utf8");
-  data = JSON.parse(data);
+  let data = JSON.parse(fs.readFileSync("db/db.json"));
 
-  if (!data) {
-    data = [];
-  }
   data.push(newNote);
 
-  fs.writeFileSync("./db/db.json", JSON.stringify(data));
-  return res.json(newNote);
+  fs.writeFileSync("db/db.json", JSON.stringify(data));
+  res.sendStatus(200);
 });
-
-//delete
-//req.params.id
 
 module.exports = router;
