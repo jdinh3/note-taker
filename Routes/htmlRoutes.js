3c0007d24ddc1e680afad8a639ef6c8c558d5aad
Jenny Dinh <jenny.p.dinh@gmail.com>
2021-04-16 20:52:03 -0500

    updated read me

--- Routes/htmlRoutes.js
+++ Routes/htmlRoutes.js
@@ -4,6 +4,10 @@ router.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
+router.get("/", function (req, res) {
+  res.json(path.join(__dirname, "../public/index.html"));
+});
+
 router.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
