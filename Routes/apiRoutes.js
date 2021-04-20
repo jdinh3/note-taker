a83f734a0537a9972ec656e85b1355a07ffc0b70
Jenny Dinh <jenny.p.dinh@gmail.com>
2021-04-20 16:07:45 -0500

    added id to notes

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
+const { SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS } = require("constants");
 const fs = require("fs");
-
+const uuid = require("uuid")
 router.get("/api/notes", (req, res) => {
   //read db.json and return it through response from server res.json()
   //data is retrieved as a string
@@ -15,11 +16,20 @@ router.post("/api/notes", (req, res) => {
   let newNote = req.body;
 
   let data = JSON.parse(fs.readFileSync("db/db.json"));
-
+  newNote.id = uuid.v4();
   data.push(newNote);
 
   fs.writeFileSync("db/db.json", JSON.stringify(data));
   res.json(data);
+});
+
+router.delete("/notes/:id", (req, res) => {
+  connection.query(
+    "DELETE FROM project WHERE id=" + req.params.id,
+    function (err, results) {
+      if (err) throw err;
+    }
+  );
 });
 
 module.exports = router;
--- Routes/htmlRoutes.js
+++ Routes/htmlRoutes.js
@@ -1,11 +1,12 @@
 const path = require("path");
 const router = require("express").Router();
+
 router.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
 router.get("/", function (req, res) {
-  res.json(path.join(__dirname, "../public/index.html"));
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 router.get("*", (req, res) => {
